e40390af75624378e12b18e637964be8c0a8da63
Pauline Muigai <wanjirumuigai@gmail.com>
2023-04-04 16:07:30 +0300

    completed code along

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,50 @@
+require "pry"
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
+  set :default_content_type, "application/json"
 
-  get '/games' do
+  get "/games" do
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
 
-  get '/games/:id' do
+  get "/games/:id" do
     game = Game.find(params[:id])
 
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name]
+            }
+          }
+        }
+      }
+    )
   end
 
+  post "/reviews" do
+    review =
+      Review.create(
+        score: params[:score],
+        comment: params[:comment],
+        game_id: params[:game_id],
+        user_id: params[:user_id]
+      )
+    review.to_json
+  end
+
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(score: params[:score], comment: params[:comment])
+    review.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
 end
